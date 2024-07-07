1a9d7ce7621d40d37c6c947113417f3197f2af42
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-07-07 08:56:50 +0700

    first commit

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Adjust paths as needed
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // Allow all methods (GET, POST, etc.)
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'], // Allow all origins (you might want to restrict this in production)
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [], // If you want to use patterns for allowed origins, specify them here
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // Allow all headers
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], // Specify which headers should be exposed
 
-    'max_age' => 0,
+    'max_age' => 0, // The maximum age of the preflight request in seconds
 
-    'supports_credentials' => false,
+    'supports_credentials' => false, // Whether or not to allow credentials (cookies, HTTP authentication, etc.)
 
 ];
